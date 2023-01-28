d81cfdf83737525e7d844ec5efec3cd8c6c1670e
Sh3yk0  (Sahid Kick) <sheyko.wk@gmail.com>
2023-01-27 18:34:23 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Instalacion:
+
+```bash
+npm install
+```
+
+
+Como ejecutar los testings??
+```bash
+npm run test
+```
+
+
+> Tu calificacion de este entregable dependera de la cantidad de tests que pases
---
+++ test/application/getAllUsersRoute.test.js
@@ -0,0 +1,28 @@
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const {describe, it} = require('mocha')
+
+const app = require('../../src/app.js')
+
+chai.use(chaiHttp)
+
+describe('Testing de la ruta de creacion de usuarios', () => {
+    it('La respuesta debe tener un status 200', (done) => {
+        chai.request(app)
+            .get('/users')
+            
+            .end((err, res) => {
+                chai.assert.equal(res.status, 200)
+                done()
+            })
+    })
+    it('La respuesta debe ser de tipo array', (done) => {
+        chai.request(app)
+            .get('/users')
+            .end((err, res) => {
+                chai.assert.typeOf(res.body, 'array')
+                done()
+            })
+    })
+})
+
